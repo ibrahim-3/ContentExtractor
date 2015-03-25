c72df09d9ddeed0c0590fd911c5029bec540b6ba
tangjaron <jaron@tangjarondeMacBook-Pro.local>
2015-03-25 20:59:56 +0800

    增加了标题的处理

--- ContentExtractor.py
+++ ContentExtractor.py
@@ -2,35 +2,47 @@
 # encoding: utf-8
 
 import requests
+from BeautifulSoup import BeautifulSoup
 import re
 
+
 class ContentExtractor(object):
-    
-    def __init__(self, url, threshold=173):
+    def __init__(self, url='', html='', threshold=173):
         self.url = url
         self.threshold = threshold
         self.plain_text = ''
         self.html_text = ''
-        self.html = ''
+        self.html = html
         self.content = ''
         self.blocks = []
         self.lines = ()
         self.len_per_lines = []
+        self.title = ''
 
     def _pre_process(self):
-        response = requests.get(self.url)
-        self.html = response.text
+        if self.html == '':
+            response = requests.get(self.url)
+            self.html = response.text
+        css_soup = BeautifulSoup(self.html)
+        if css_soup.find('h1') is not None:
+            self.title = css_soup.find('h1').text
+        elif css_soup.find('title') is not None:
+            self.title = css_soup.find('title').text
+        else:
+            return
         plain_text, html_text = self._clean_html(self.html)
         self.plain_text = plain_text
         self.html_text = html_text
 
     def _extract(self, threshold=173):
+        if self.plain_text is '':
+            return
         self.lines = tuple(self.plain_text.split('\n'))
-        self.len_per_lines = [len(re.sub(r'\s*', '', self.lines[i])) for i in range(len(self.lines))]
-        #for i, v in enumerate(self.len_per_lines):
-        #    print i+1, v
+        for i in range(len(self.lines)):
+            length = len(re.sub(r'\s*', '', self.lines[i]))
+            self.len_per_lines.append(length)
         self._caculate_block(3)
-        start =0
+        start = 0
         end = 0
         while True:
             start = self._find_surge(end, threshold)
@@ -41,9 +53,9 @@ class ContentExtractor(object):
         self.content = re.sub(r'\n', '[crlf]', self.content)
         self.content = re.sub(r'\s*', '', self.content)
         self.content = re.sub(r'(?:\[crlf\])+', '\n', self.content)
-        print self.content
-            
-        
+        if len(self.content) == 0:
+            self.title = ''
+
     def _find_surge(self, start, threshold):
         for i in range(start, len(self.blocks) - 2):
             if self.blocks[i] > threshold\
@@ -82,5 +94,9 @@ class ContentExtractor(object):
         self._extract()
 
 if __name__ == '__main__':
-    ce = ContentExtractor('http://www.csdn.net/article/2015-02-26/2824020')
+    # ce = ContentExtractor('http://www.csdn.net/article/2015-02-26/2824020')
+    ce = ContentExtractor('http://beiyuu.com/religion-basic/')
+    # ce = ContentExtractor('http://beiyuu.com')
     ce.execute()
+    print ce.title
+    print ce.content
